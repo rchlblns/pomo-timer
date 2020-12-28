2c0f55e503648835e9b670637125d31cf913b202
Short Stack <41342213+rchlblns@users.noreply.github.com>
2020-12-28 11:53:46 -0600

    routing change

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const path = require("path");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-// test route to backend
-app.get("/test", (req, res) => {
-    res.send("Hello from the backend of this app!");
-});
-
 // serve static files
 app.use(express.static(path.join(__dirname, "../client/build")));
+
+// test route to backend
+app.use("/test", (req, res) => {
+    res.send("Hello from the backend of this app!");
+});
 
 // handle React routing, return all requests to React app
 app.get("/", (req, res) => {
